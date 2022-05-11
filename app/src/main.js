940a8a2c681297fd94a8f98278f5cac5ddaf9827
Kurja <Kurja0403@gmail.com>
2022-05-11 23:51:11 +0800

    配置路由（有错误待检查）

--- app/src/main.js
+++ app/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-Vue.config.productionTip = false
-
+// 引入路由
+import router from '@/router';
 new Vue({
   render: h => h(App),
+  // 注册路由：底下的写法 KV 一致省略 V [router 小写的]
+  router
 }).$mount('#app')
+Vue.config.productionTip = false
